741d01dfe02d8eee6b98246806c02a7565497601
Diana Mora <morra.dianna@gmail.com>
2020-11-06 17:24:48 -0500

    commit tree dc3d136699085dddbf393e735e7aa988f6a8b153

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+# def sort
+#     self.roster.each do |grade, student_arr|
+#         student_arr.sort 
+#     end
+# end
+
+class School 
+    attr_accessor :school_name, :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {} 
+    end 
+
+    def add_student(student, grade)
+        if roster.has_key?(grade) 
+            roster[grade] << student 
+        else roster[grade] = [student]
+        # if @roster[grade] == nil
+        #     @roster[grade] = []
+        # end
+        # @roster[grade] << student
+        end 
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        @roster.each do |grade, student|
+           student.sort!
+        end
+    end
+
+end
+
